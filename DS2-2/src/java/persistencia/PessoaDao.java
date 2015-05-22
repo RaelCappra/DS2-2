2260f04132573ee7662264ccf4a89e7a7c50a058
Rael Cappra <rael.cappra@gmail.com>
2015-05-22 16:04:59 -0300

    adicionado PessoaDao.listAll()

--- DS2-2/src/java/persistencia/PessoaDao.java
+++ DS2-2/src/java/persistencia/PessoaDao.java
@@ -2,10 +2,13 @@ package persistencia;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.Dependente;
 import model.Pessoa;
 
 public class PessoaDao implements Dao<Pessoa, Long> {
@@ -16,7 +19,7 @@ public class PessoaDao implements Dao<Pessoa, Long> {
     public void save(Pessoa entity) {
         String query = "insert into pessoa (nome, sobrenome) values (?, ?)";
         try {
-            if(conexao == null){
+            if (conexao == null) {
                 conexao = new ConexaoPostgreSQL("localhost", "postgres", "postgres", "postgres");
             }
             try (Connection connection = conexao.getConnection();
@@ -24,8 +27,7 @@ public class PessoaDao implements Dao<Pessoa, Long> {
                 ps.setString(1, entity.getNome());
                 ps.setString(2, entity.getSobrenome());
                 ps.execute();
-            }
-            catch(SQLException e){
+            } catch (SQLException e) {
                 //TODO: ERRO: nao foi adicionada a pessoa
             }
         } catch (Exception ex) {
@@ -55,8 +57,31 @@ public class PessoaDao implements Dao<Pessoa, Long> {
 
     @Override
     public List<Pessoa> listAll() {
-        //TODO: implementar listAll
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String query = "select * from dependente";
+        List<Pessoa> result = new ArrayList<>();
+        try {
+            if (conexao == null) {
+                conexao = new ConexaoPostgreSQL("localhost", "postgres", "postgres", "postgres");
+            }
+            try (Connection connection = conexao.getConnection();
+                    PreparedStatement ps = connection.prepareStatement(query)) {
+                ResultSet rs = ps.executeQuery();
+
+                while (rs.next()) {
+                    Pessoa pessoa = new Pessoa();
+                    pessoa.setId(rs.getLong("id"));
+                    pessoa.setNome(rs.getString("nome"));
+                    pessoa.setSobrenome(rs.getString("sobrenome"));
+                    result.add(pessoa);
+                }
+            } catch (SQLException e) {
+                //TODO: ERRO: nao foram listados os dependentes
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(PessoaDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return result;
     }
 
     @Override
