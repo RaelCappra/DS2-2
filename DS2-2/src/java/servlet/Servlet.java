b89780562f08294c6df11dd6d36d2f59bc135cb5
Rael Cappra <rael.cappra@gmail.com>
2015-05-22 16:26:04 -0300

    adicionada implementacao para action de listagem na Servlet

--- DS2-2/src/java/servlet/Servlet.java
+++ DS2-2/src/java/servlet/Servlet.java
@@ -7,11 +7,15 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Pessoa;
+import persistencia.PessoaDao;
 
 /**
  *
@@ -32,17 +36,15 @@ public class Servlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Servlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Servlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String action = request.getParameter("action");
+        switch (action){
+            case("listarPessoas"):{
+                PessoaDao pessoaDao = new PessoaDao();
+                List<Pessoa> pessoas = pessoaDao.listAll();
+                request.setAttribute("pessoas", pessoas);
+                RequestDispatcher rd = request.getRequestDispatcher("listagem.jsp");
+                rd.forward(request, response);
+            }
         }
     }
 
