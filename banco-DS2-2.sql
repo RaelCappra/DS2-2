4ec4b97a83a1a988d4175afbe2c73c45fb4456c8
RaelCappra <rael.cappra@gmail.com>
2015-05-26 07:30:09 -0300

    banco agora usa not null em nome e sobrenome de pessoas e dependentes

--- banco-DS2-2.sql
+++ banco-DS2-2.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.3.5
 -- Dumped by pg_dump version 9.3.1
--- Started on 2015-05-17 12:26:51
+-- Started on 2015-05-26 07:28:35
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -63,14 +63,14 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 173 (class 1259 OID 49180)
+-- TOC entry 170 (class 1259 OID 49194)
 -- Name: dependente; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
 CREATE TABLE dependente (
     id integer NOT NULL,
-    nome character varying,
-    sobrenome character varying,
+    nome character varying NOT NULL,
+    sobrenome character varying NOT NULL,
     pessoa integer NOT NULL
 );
 
@@ -78,7 +78,7 @@ CREATE TABLE dependente (
 ALTER TABLE public.dependente OWNER TO postgres;
 
 --
--- TOC entry 172 (class 1259 OID 49178)
+-- TOC entry 171 (class 1259 OID 49200)
 -- Name: Dependente_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -94,7 +94,7 @@ ALTER TABLE public."Dependente_id_seq" OWNER TO postgres;
 
 --
 -- TOC entry 1960 (class 0 OID 0)
--- Dependencies: 172
+-- Dependencies: 171
 -- Name: Dependente_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -102,21 +102,21 @@ ALTER SEQUENCE "Dependente_id_seq" OWNED BY dependente.id;
 
 
 --
--- TOC entry 171 (class 1259 OID 49169)
+-- TOC entry 172 (class 1259 OID 49202)
 -- Name: pessoa; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
 CREATE TABLE pessoa (
     id integer NOT NULL,
-    nome character varying,
-    sobrenome character varying
+    nome character varying NOT NULL,
+    sobrenome character varying NOT NULL
 );
 
 
 ALTER TABLE public.pessoa OWNER TO postgres;
 
 --
--- TOC entry 170 (class 1259 OID 49167)
+-- TOC entry 173 (class 1259 OID 49208)
 -- Name: Pessoa_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -132,7 +132,7 @@ ALTER TABLE public."Pessoa_id_seq" OWNER TO postgres;
 
 --
 -- TOC entry 1961 (class 0 OID 0)
--- Dependencies: 170
+-- Dependencies: 173
 -- Name: Pessoa_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -140,7 +140,7 @@ ALTER SEQUENCE "Pessoa_id_seq" OWNED BY pessoa.id;
 
 
 --
--- TOC entry 1833 (class 2604 OID 49183)
+-- TOC entry 1832 (class 2604 OID 49210)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -148,7 +148,7 @@ ALTER TABLE ONLY dependente ALTER COLUMN id SET DEFAULT nextval('"Dependente_id_
 
 
 --
--- TOC entry 1832 (class 2604 OID 49172)
+-- TOC entry 1833 (class 2604 OID 49211)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -157,40 +157,40 @@ ALTER TABLE ONLY pessoa ALTER COLUMN id SET DEFAULT nextval('"Pessoa_id_seq"'::r
 
 --
 -- TOC entry 1962 (class 0 OID 0)
--- Dependencies: 172
+-- Dependencies: 171
 -- Name: Dependente_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('"Dependente_id_seq"', 3, true);
+SELECT pg_catalog.setval('"Dependente_id_seq"', 50, true);
 
 
 --
 -- TOC entry 1963 (class 0 OID 0)
--- Dependencies: 170
+-- Dependencies: 173
 -- Name: Pessoa_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('"Pessoa_id_seq"', 3, true);
+SELECT pg_catalog.setval('"Pessoa_id_seq"', 27, true);
 
 
 --
--- TOC entry 1949 (class 0 OID 49180)
--- Dependencies: 173
+-- TOC entry 1946 (class 0 OID 49194)
+-- Dependencies: 170
 -- Data for Name: dependente; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 
 
 --
--- TOC entry 1947 (class 0 OID 49169)
--- Dependencies: 171
+-- TOC entry 1948 (class 0 OID 49202)
+-- Dependencies: 172
 -- Data for Name: pessoa; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 
 
 --
--- TOC entry 1835 (class 2606 OID 49177)
+-- TOC entry 1837 (class 2606 OID 49213)
 -- Name: Pessoa_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -199,7 +199,7 @@ ALTER TABLE ONLY pessoa
 
 
 --
--- TOC entry 1837 (class 2606 OID 49188)
+-- TOC entry 1835 (class 2606 OID 49215)
 -- Name: dependente_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -208,7 +208,7 @@ ALTER TABLE ONLY dependente
 
 
 --
--- TOC entry 1838 (class 2606 OID 49189)
+-- TOC entry 1838 (class 2606 OID 49216)
 -- Name: dependente_pessoa_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -228,7 +228,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2015-05-17 12:26:51
+-- Completed on 2015-05-26 07:28:35
 
 --
 -- PostgreSQL database dump complete
