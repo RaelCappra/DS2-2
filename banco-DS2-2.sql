042b6215ea749eaf5ee3b70f1e60d432196eb9a8
RaelCappra <rael.cappra@gmail.com>
2015-05-26 07:59:03 -0300

    regex no banco

--- banco-DS2-2.sql
+++ banco-DS2-2.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.3.5
 -- Dumped by pg_dump version 9.3.1
--- Started on 2015-05-26 07:28:35
+-- Started on 2015-05-26 07:58:47
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -14,8 +14,8 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
--- TOC entry 1955 (class 1262 OID 12029)
--- Dependencies: 1954
+-- TOC entry 1959 (class 1262 OID 12029)
+-- Dependencies: 1958
 -- Name: postgres; Type: COMMENT; Schema: -; Owner: postgres
 --
 
@@ -31,7 +31,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 1958 (class 0 OID 0)
+-- TOC entry 1962 (class 0 OID 0)
 -- Dependencies: 175
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -48,7 +48,7 @@ CREATE EXTENSION IF NOT EXISTS adminpack WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 1959 (class 0 OID 0)
+-- TOC entry 1963 (class 0 OID 0)
 -- Dependencies: 174
 -- Name: EXTENSION adminpack; Type: COMMENT; Schema: -; Owner: 
 --
@@ -63,7 +63,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 170 (class 1259 OID 49194)
+-- TOC entry 170 (class 1259 OID 49253)
 -- Name: dependente; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -71,14 +71,16 @@ CREATE TABLE dependente (
     id integer NOT NULL,
     nome character varying NOT NULL,
     sobrenome character varying NOT NULL,
-    pessoa integer NOT NULL
+    pessoa integer NOT NULL,
+    CONSTRAINT dependente_nome_letras CHECK (((nome)::text ~ '^[a-zA-Z]+$'::text)),
+    CONSTRAINT dependtente_sobrenome_letras_espacos CHECK (((sobrenome)::text ~ '^[a-zA-Z][a-zA-Z ]+[a-zA-Z]$|^[a-zA-Z]$'::text))
 );
 
 
 ALTER TABLE public.dependente OWNER TO postgres;
 
 --
--- TOC entry 171 (class 1259 OID 49200)
+-- TOC entry 171 (class 1259 OID 49259)
 -- Name: Dependente_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -93,7 +95,7 @@ CREATE SEQUENCE "Dependente_id_seq"
 ALTER TABLE public."Dependente_id_seq" OWNER TO postgres;
 
 --
--- TOC entry 1960 (class 0 OID 0)
+-- TOC entry 1964 (class 0 OID 0)
 -- Dependencies: 171
 -- Name: Dependente_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -102,21 +104,23 @@ ALTER SEQUENCE "Dependente_id_seq" OWNED BY dependente.id;
 
 
 --
--- TOC entry 172 (class 1259 OID 49202)
+-- TOC entry 172 (class 1259 OID 49261)
 -- Name: pessoa; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 
 CREATE TABLE pessoa (
     id integer NOT NULL,
     nome character varying NOT NULL,
-    sobrenome character varying NOT NULL
+    sobrenome character varying NOT NULL,
+    CONSTRAINT pessoa_nome_letras CHECK (((nome)::text ~ '^[a-zA-Z]+$'::text)),
+    CONSTRAINT pessoa_sobrenome_letras_espacos CHECK (((sobrenome)::text ~ '^[a-zA-Z][a-zA-Z ]+[a-zA-Z]$|^[a-zA-Z]$'::text))
 );
 
 
 ALTER TABLE public.pessoa OWNER TO postgres;
 
 --
--- TOC entry 173 (class 1259 OID 49208)
+-- TOC entry 173 (class 1259 OID 49269)
 -- Name: Pessoa_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -131,7 +135,7 @@ CREATE SEQUENCE "Pessoa_id_seq"
 ALTER TABLE public."Pessoa_id_seq" OWNER TO postgres;
 
 --
--- TOC entry 1961 (class 0 OID 0)
+-- TOC entry 1965 (class 0 OID 0)
 -- Dependencies: 173
 -- Name: Pessoa_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -140,7 +144,7 @@ ALTER SEQUENCE "Pessoa_id_seq" OWNED BY pessoa.id;
 
 
 --
--- TOC entry 1832 (class 2604 OID 49210)
+-- TOC entry 1832 (class 2604 OID 49271)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -148,7 +152,7 @@ ALTER TABLE ONLY dependente ALTER COLUMN id SET DEFAULT nextval('"Dependente_id_
 
 
 --
--- TOC entry 1833 (class 2604 OID 49211)
+-- TOC entry 1835 (class 2604 OID 49272)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -156,25 +160,25 @@ ALTER TABLE ONLY pessoa ALTER COLUMN id SET DEFAULT nextval('"Pessoa_id_seq"'::r
 
 
 --
--- TOC entry 1962 (class 0 OID 0)
+-- TOC entry 1966 (class 0 OID 0)
 -- Dependencies: 171
 -- Name: Dependente_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('"Dependente_id_seq"', 50, true);
+SELECT pg_catalog.setval('"Dependente_id_seq"', 52, true);
 
 
 --
--- TOC entry 1963 (class 0 OID 0)
+-- TOC entry 1967 (class 0 OID 0)
 -- Dependencies: 173
 -- Name: Pessoa_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('"Pessoa_id_seq"', 27, true);
+SELECT pg_catalog.setval('"Pessoa_id_seq"', 45, true);
 
 
 --
--- TOC entry 1946 (class 0 OID 49194)
+-- TOC entry 1950 (class 0 OID 49253)
 -- Dependencies: 170
 -- Data for Name: dependente; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -182,15 +186,16 @@ SELECT pg_catalog.setval('"Pessoa_id_seq"', 27, true);
 
 
 --
--- TOC entry 1948 (class 0 OID 49202)
+-- TOC entry 1952 (class 0 OID 49261)
 -- Dependencies: 172
 -- Data for Name: pessoa; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
+INSERT INTO pessoa VALUES (45, 'aa', 'a');
 
 
 --
--- TOC entry 1837 (class 2606 OID 49213)
+-- TOC entry 1841 (class 2606 OID 49274)
 -- Name: Pessoa_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -199,7 +204,7 @@ ALTER TABLE ONLY pessoa
 
 
 --
--- TOC entry 1835 (class 2606 OID 49215)
+-- TOC entry 1839 (class 2606 OID 49276)
 -- Name: dependente_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -208,7 +213,7 @@ ALTER TABLE ONLY dependente
 
 
 --
--- TOC entry 1838 (class 2606 OID 49216)
+-- TOC entry 1842 (class 2606 OID 49277)
 -- Name: dependente_pessoa_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -217,7 +222,7 @@ ALTER TABLE ONLY dependente
 
 
 --
--- TOC entry 1957 (class 0 OID 0)
+-- TOC entry 1961 (class 0 OID 0)
 -- Dependencies: 6
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -228,7 +233,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2015-05-26 07:28:35
+-- Completed on 2015-05-26 07:58:47
 
 --
 -- PostgreSQL database dump complete
